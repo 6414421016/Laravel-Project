ff7d2dda2bef24b2f1a9e093f6a2776694ee2128
6414421016 <117976265+6414421016@users.noreply.github.com>
2023-11-13 10:32:49 +0700

    Add files via upload

---
+++ w02/views/bio.blade.php
@@ -0,0 +1,13 @@
+<!-- resources/views/bio.blade.php -->
+
+@extends('layouts.app')
+
+@section('title', 'Bio')
+
+@section('content')
+    <div class="container mx-auto mt-8">
+        <h1 class="text-4xl font-bold mb-4">My Bio</h1>
+        <p class="text-lg">Warathep Tanyaruk 6414421016 RBRU</p>
+        <!-- Add more content as needed -->
+    </div>
+@endsection
---
+++ w02/views/home.blade.php
@@ -0,0 +1,13 @@
+<!-- resources/views/home.blade.php -->
+
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <div class="container mx-auto mt-8">
+        <h1 class="text-4xl font-bold mb-4">Welcome to the Home Page!</h1>
+        <p class="text-lg">Welcome to Terraria House</p>
+        <!-- Add more content as needed -->
+    </div>
+@endsection
